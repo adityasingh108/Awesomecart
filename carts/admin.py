dfce01b4471f94ebd31d61ae36a090a75d360c9d
adityasingh108 <adityasingh78085@gmail.com>
2021-09-07 01:46:55 +0530

    adding variatation

--- carts/admin.py
+++ carts/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product','cart','quantity','is_active')
+    
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id','date_added')    
+
+admin.site.register(Cart,CartAdmin)
+admin.site.register(CartItem ,CartItemAdmin)
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Product
+from store.models import Product ,Variation
 from .models  import Cart,CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -13,6 +14,22 @@ def _cart_id(request):
 
 def add_cart(request ,product_id):
     product = Product.objects.get(id=product_id) # get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(product=product,variation_category__iexact=key,variation_value__iexact=value)
+                product_variation.append(variation)
+                print(product_variation)
+            except:
+                pass
+        
+            
+            
+    
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))# get the cart using the cart id  is prent in the session
     except Cart.DoesNotExist:
