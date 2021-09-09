80f2dde17d19534d21f3b9e3a574733bf9b2616a
adityasingh108 <adityasingh78085@gmail.com>
2021-09-09 14:30:03 +0530

    ACCOUNT ACTIVATION FOR USER

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,21 @@
 from django import contrib
 from django.contrib import messages,auth
+from django.http.response import HttpResponse
 from django.shortcuts import render ,redirect
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# user CAtivation
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 def register(request):
     if request.method=='POST':
@@ -20,8 +30,23 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request,"Registration​  Successful")
-            return redirect('register')
+             
+            # user Activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verifivcation_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
+             
+            # messages.success(request,"Thanku for registering with us. We have sent to a verification link to your email adress. please verify it.")
+            
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -51,3 +76,20 @@ def logout(request):
     auth.logout(request)
     messages.success(request ,'You are logged Out')
     return redirect('login')
+
+def activate(request ,uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user= None
+    
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'Congratulation. your account has been avtivated')
+        return redirect('login')
+    else:
+        messages.error(request,"Invalid activation link")
+        return redirect('register')
+            
--- awsomecart/settings.py
+++ awsomecart/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jackrayan359@gmail.com'
+EMAIL_HOST_PASSWORD = '@@jackrayan##'
